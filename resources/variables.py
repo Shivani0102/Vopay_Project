99f0483d621fc7a6e837778ee5ea6a8b8e4f057b
Shivani0102 <shiviraj71@gmail.com>
2020-10-19 16:54:55 +0530

    chal yr push hoja ab

---
+++ resources/variables.py
@@ -0,0 +1,19 @@
+"""
+This file contains all the configurable variables used
+by the test cases.
+"""
+
+import random
+
+randNum = str(random.randrange(100000, 999999))
+bankName= "Tanger"
+user="Hamedarbabioon"
+pwd = "051979"
+value = "chacha"
+TanName = "Tan"
+invalidUser = "user" + randNum
+validuser = "Greatday"
+invalidPass ="pass"+ randNum
+validpass ="Everyday"
+flinkName="flink"
+desired_url="https://www.tangerine.ca/app/#/"
